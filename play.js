4f5da05ef0f3618ad8ec0a5ab8aa0af76873f726
Jubril Bello <jbello@uoguelph.ca>
2023-06-17 21:39:15 -0400

    implement ES6 shorthand syntax

--- play.js
+++ play.js
@@ -1,16 +1,4 @@
-const net = require("net");
-
-// Establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting ...");
 const sConn = connect();
