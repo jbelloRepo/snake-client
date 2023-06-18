5bba078e18a71b820b637825fa382494028b7a8c
Jubril Bello <jbello@uoguelph.ca>
2023-06-17 21:55:39 -0400

    Add initials when connection is established

--- client.js
+++ client.js
@@ -9,7 +9,19 @@ const connect = () => {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  // Register a "connect" handler
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server");
+    conn.write("Name: JBL"); // send the name to servers
+  });
+
+  // Event handler for "end" event
+  conn.on("end", () => {
+    console.log("Connection ended");
+  });
+
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
