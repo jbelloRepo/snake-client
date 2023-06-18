812fc9a196f69434dba829fcd564c3319909c149
Jubril Bello <jbello@uoguelph.ca>
2023-06-18 05:57:08 -0400

    Move setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,31 +1,13 @@
-const {connect} = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // connect to the sever
 console.log("Connecting ...");
 const sConn = connect();
 
-
 // return any message from the sever
 sConn.on("data", (data) => {
   console.log("Server says:", data);
 });
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  //event handler for stdin
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') { // Check for Ctrl + C input
-    process.exit(); // Terminate the game
-  }
-};
 
 setupInput();
