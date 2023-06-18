067cd007cb0baa7c85e4445dd644f4a86957670f
Jubril Bello <jbello@uoguelph.ca>
2023-06-18 05:48:18 -0400

    Add functions to allow reading keyboard commands

--- play.js
+++ play.js
@@ -1,8 +1,31 @@
 const {connect} = require("./client");
 
+// connect to the sever
 console.log("Connecting ...");
 const sConn = connect();
 
+
+// return any message from the sever
 sConn.on("data", (data) => {
   console.log("Server says:", data);
 });
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //event handler for stdin
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') { // Check for Ctrl + C input
+    process.exit(); // Terminate the game
+  }
+};
+
+setupInput();
