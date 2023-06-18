c217d3889b5f6050ba12aaa0962b5b17b163559f
Jubril Bello <jbello@uoguelph.ca>
2023-06-18 06:09:16 -0400

    Add WASD movement functionality

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,7 +17,15 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     // Check for Ctrl + C input
     process.exit(); // Terminate the game
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
-module.exports = {setupInput};
+module.exports = { setupInput };
